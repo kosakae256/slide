58d85cd7e941db7733052e65be33d8d068b59f46
kosakae256 <kosaka256@gmail.com>
2023-02-06 16:34:15 +0900

    fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,30 +29,31 @@ const PdfPage = () => {
   return (
     <>
       <div className="w-screen h-screen flex flex-col">
-        <div className="w-full h-16 bg-theme">
+        <div className="w-full h-16 bg-white border-b py-2 pl-2">
+          <img src="./logo.png" className="h-full" />
           <h2 className="h-full flex items-center text-white font-bold pl-4 text-3xl">Slide</h2>
         </div>
 
         <div className="w-full h-2 flex-auto flex flex-row">
-          <div className="bg-white w-40 h-full flex flex-col">
+          <div className="bg-white w-48 h-full flex flex-col">
             <div className="w-full h-2 flex-auto overflow-y-auto p-2 space-y-2">
               {imgs.map((v, i) => {
                 return (
                   <button className={`border w-full h-auto relative ${i == page && "border-theme border-2"} hover:border-2 hover:border-gray-500`} onClick={() => pageChange(i)} key={v}>
                     <img src={v} />
-                    <p className="absolute bottom-0 right-1 font-bold text-theme">{i + 1}</p>
+                    <p className="absolute bottom-0 right-1">{i + 1}</p>
                   </button>
                 )
               })}
             </div>
-            <div className="w-40 h-40 border-t flex flex-col items-center justify-center">
-              <img src="qrcode.png" className="w-32 h-32" />
+            <div className="w-48 h-48 border-t flex flex-col items-center justify-center">
+              <img src="qrcode.png" className="w-36 h-36" />
               <p className="font-bold">このスライドにアクセス</p>
             </div>
           </div>
 
           <div className="h-full w-2 flex-auto bg-gray-50 border-x relative">
-            <img src={imgs[page]} className="h-full w-full object-contain border-y" />
+            <img src={imgs[page]} className="h-full w-full object-contain" />
             <button className="absolute right-0 inset-y-0 h-full hover:bg-gray-500 hover:bg-opacity-10" onClick={() => pageChange(page + 1)}>
               <IoCaretForward className="h-full text-theme" fontSize="2rem" />
             </button>
